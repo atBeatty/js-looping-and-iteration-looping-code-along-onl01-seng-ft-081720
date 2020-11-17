ad23aab9e3b3a696b31ab3d17f50569bc60669e4
atBeatty <adam.t.beatt@gmail.com>
2020-11-17 10:09:24 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(arr, str) {
+    let messageArray = []
+    for(let i=0; i<arr.length; i++) {
+    messageArray.push((`Thank you, ${arr[i]}, for the wonderful ${str} gift!`))
+    }
+return messageArray;
+}
+
+function countDown(num) {
+    for(let i=num; i>=0; i--){
+        console.log(i)
+        
+    }
+}
